6880bb3cf885d3b2d22582aa9af72a53966b1e6b
孟戈 <2631994831@qq.com>
2023-12-01 08:43:11 +0000

    21301013-孟戈

---
+++ src/trap/mod.rs
@@ -0,0 +1,82 @@
+use core::arch::global_asm;
+
+global_asm!(include_str!("trap.S"));
+
+pub fn init() {
+    extern "C" { fn __alltraps(); }
+    unsafe {
+        stvec::write(__alltraps as usize, TrapMode::Direct);
+    }
+}
+
+pub mod context;
+
+use riscv::register::{
+    mtvec::TrapMode,
+    stvec,
+    scause::{
+        self,
+        Trap,
+        Exception,
+    },
+    stval,
+};
+
+use crate::syscall::syscall;
+//use crate::batch::run_next_app;
+
+#[no_mangle]
+pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
+    let scause = scause::read();
+    let stval = stval::read();
+    match scause.cause() {
+        Trap::Exception(Exception::UserEnvCall) => {
+            cx.sepc += 4;
+            cx.x[10] = syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]) as usize;
+        }
+        Trap::Exception(Exception::StoreFault) |
+        Trap::Exception(Exception::StorePageFault) => {
+            println!("[kernel] PageFault in application, core dumped.");
+//            run_next_app();
+        }
+        Trap::Exception(Exception::IllegalInstruction) => {
+            println!("[kernel] IllegalInstruction in application, core dumped.");
+//            run_next_app();
+        }
+        _ => {
+            panic!("Unsupported trap {:?}, stval = {:#x}!", scause.cause(), stval);
+        }
+    }
+    cx
+}
+
+pub use context::TrapContext;
+
+use riscv::register::{
+
+
+    scause::{
+        
+        
+        
+        Interrupt,
+    },
+    
+    sie,
+};
+
+use crate::task::{
+    exit_current_and_run_next,
+    suspend_current_and_run_next,
+};
+
+use crate::timer::set_next_trigger;
+
+
+pub fn enable_timer_interrupt() {
+    unsafe { sie::set_stimer(); }
+}
+
+
+
+
